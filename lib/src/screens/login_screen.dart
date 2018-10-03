f4f5cd39c0b0b512d1ebe7f782dc2df9abd39879
Shola Adeshina Hassan <delameh@icloud.com>
2018-10-03 05:54:15 +0100

    first commit

---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import '../blocs/bloc.dart';
+
+class LoginScreen extends StatelessWidget{
+
+  Widget build(context){
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          emailField(),
+          Container(margin: EdgeInsets.only(top: 20.0)),
+          passwordField(),
+          Container(margin: EdgeInsets.only(top: 20.0)),
+          submitButton(),
+        ],
+      )
+    );
+  }
+
+  Widget emailField(){
+    return StreamBuilder(
+      stream: bloc.email,
+      builder: (context, snapshot){
+        return TextField(
+              onChanged: bloc.changeEmail,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                hintText: 'shola@hotmail.com',
+                labelText: 'Email Address',
+                errorText: snapshot.error
+              ),
+        );
+      }
+    );
+  }
+
+  Widget passwordField(){
+    return StreamBuilder(
+      stream: bloc.password,
+      builder: (context, snapshot){
+        return TextField(
+              onChanged: bloc.changePassword,
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: 'Password',
+                labelText: 'Password',
+                errorText: snapshot.error
+              ),
+        );
+      }
+    );
+  }  
+
+  Widget submitButton(){
+     return RaisedButton(
+       child: Text('Login'),
+       color: Colors.blue,
+       onPressed: () {
+
+       }
+     );
+  }
+  
+
+
+}
